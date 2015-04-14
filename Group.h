222ac38f5b827b182e985d70c4f87bc699d9cfb2
Joanne Cuffey <joannecuffey@gmail.com>
2015-04-14 19:15:11 +0100

    /

---
+++ Group.h
@@ -0,0 +1,16 @@
+//
+//  Group.h
+//  Divided
+//
+//  Created by Jo on 19/03/2015.
+//  Copyright (c) 2015 Jo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Group : NSObject
+
+@property (nonatomic, strong) NSString *groupID;
+@property (nonatomic, strong) NSString *groupName;
+
+@end
---
+++ Group.m
@@ -0,0 +1,13 @@
+//
+//  Group.m
+//  Divided
+//
+//  Created by Jo on 19/03/2015.
+//  Copyright (c) 2015 Jo. All rights reserved.
+//
+
+#import "Group.h"
+
+@implementation Group
+
+@end
